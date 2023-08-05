6c8592d941086a61507805067f7d4af52663c35d
Hamza <6793siddique@gmail.com>
2023-08-06 01:16:39 +0500

    Test Routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
-use App\Http\Controllers\BlockedUserController;
-use App\Http\Controllers\LeadvertexOrdersController;
 use App\Http\Controllers\WebhookController;
-use App\Http\Controllers\WebshippyOrdersController;
 use Illuminate\Support\Facades\Route;
 use Spatie\DiscordAlerts\Facades\DiscordAlert;
 
@@ -26,27 +23,5 @@ Route::get('/test', function () {
 });
 
 Route::controller(WebhookController::class)->group(function () {
-    Route::post('leadvertex/webhook', 'store'); // LV status: ACCEPTED
-    Route::post('leadvertex-all-orders/webhook', 'createRecordOnComnica'); // LV New Order
-    // Route::get('createRecordOnComnica', 'sendData');
-
+    Route::post('shopify-new-order', 'shopify_new_order'); // Shopify New Order
 });
-
-Route::controller(WebshippyOrdersController::class)->group(function () {
-    Route::get('get_webshippy_orders', 'UpdateOrders');
-    Route::get('/chart-data', 'chartData')->name('chart.data');
-});
-
-Route::controller(BlockedUserController::class)->group(function () {
-    Route::get('blocked_users', 'index');
-    Route::get('block/{id}', 'blockUser');
-});
-
-Route::controller(LeadvertexOrdersController::class)->group(function () {
-    Route::post('orders', 'store');
-    Route::get('thankyou', 'thankyou');
-});
-
-Route::get('/chart', function () {
-    return view('chart');
-})->name('chart');
